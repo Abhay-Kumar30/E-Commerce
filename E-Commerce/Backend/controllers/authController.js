7f03197eb5dc8ac862695cb35d19342d3e341bd5
Abhay Kumar <166091833+Abhay-Kumar30@users.noreply.github.com>
2024-05-07 12:31:30 +0530

    Update authController.js

--- E-Commerce/Backend/controllers/authController.js
+++ E-Commerce/Backend/controllers/authController.js
@@ -2,6 +2,7 @@ import userModel from "../models/userModel.js";
 import { comparePassword, hashPassword } from "./../helpers/authHelper.js";
 import JWT from "jsonwebtoken";
 
+
 // registeration of new user
 export const registerController = async (req, res) => {
   try {
